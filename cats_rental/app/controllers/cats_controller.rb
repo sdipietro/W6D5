ba82d574fa6ef67fd95dba75273dd387f016aea2
Stephen DiPietro <stevedipietro97@gmail.com>
2020-01-03 16:29:17 -0500

    Added edit.html.erb

---
+++ cats_rental/app/controllers/cats_controller.rb
@@ -0,0 +1,38 @@
+class CatsController < ApplicationController
+
+    def index
+        @cats = Cat.all
+        render :index
+    end
+
+    def show
+        @cat = Cat.find(params[:id])
+        render :show
+    end
+
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+
+        if @cat.save
+            redirect_to cat_url(@cat.id)
+        else
+            render :new, status: 422
+        end
+    end
+
+    def edit
+        @cat = Cat.find(params[:id])
+        render :edit
+    end
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:name, :birthdate, :color, :description, :sex)
+    end
+end
---
+++ cats_rental/app/models/cat.rb
@@ -0,0 +1,39 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  birthdate   :date             not null
+#  color       :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'action_view'
+require 'action_view/helpers'
+
+class Cat < ApplicationRecord
+
+    COLORS = ["black", "white", "calico", "tabby", "gray", "orange"]
+    
+    def self.colors
+        COLORS
+    end
+
+    include ActionView::Helpers::DateHelper
+
+    validates :name, :birthdate, :color, :sex, :description, presence: true
+    validates :color, inclusion: { in: Cat.colors, message: "%{value} is not a valid color" }
+    validates :sex, inclusion: { in: %w(M F), message: "We're not that progressive. Please include either M or F."}
+
+
+    def age
+        age = Date.today.year - self.birthdate.year
+        age -= 1 if Date.today < self.birthdate + age.years
+        return age
+    end
+
+end
